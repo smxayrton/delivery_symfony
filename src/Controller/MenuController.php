a93b84a7cbeb7b05802e338077eda11a635e322b
Ayrton Sousa <ayrton@samex.online>
2019-07-31 12:11:08 -0300

    some modifications on the html and Controller code

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -12,8 +12,30 @@ class MenuController extends AbstractController{
     * @Route("/menu/pizzas")
     */
     public function pizzas()
-    {
-        return $this->render('views/menu/menu.html.twig');
+    {   
+        $pizzaInfo = Array(
+            "pizza" => Array(
+                Array(
+                    "name" => "Pepperoni",
+                    "price_sm" => 10,
+                    "price_md" => 15,
+                    "price_lg" => 20,
+                    "price_xl" => 25,
+                ),
+                Array(
+                    "name" => "Other",
+                    "price_sm" => 10,
+                    "price_md" => 15,
+                    "price_lg" => 20,
+                    "price_xl" => 25,
+                )
+            )
+        );
+
+
+        return $this->render('views/menu/menu.html.twig',[
+            'pizza_info' => $pizzaInfo
+        ]);
     }
 }
 ?>
